0cf27073ad5d558f54e04e78cc2f63b2855891de
infoyuusuke-afk <info.yuusuke@gmail.com>
2026-09-22 00:34:24 +0900

    Lock Execution Permission timezone semantics

---
+++ tests/test_execution_permission_timezone.py
@@ -0,0 +1,12 @@
+import unittest
+from datetime import datetime,tzinfo
+import scripts.execution_permission as ep
+class NullOffsetTZ(tzinfo):
+ def utcoffset(self,dt): return None
+ def dst(self,dt): return None
+class ExecutionPermissionTimezoneTest(unittest.TestCase):
+ def test_parse_aware_rejects_naive(self): self.assertIsNone(ep._parse_aware("2026-09-21T15:00:00"))
+ def test_parse_aware_accepts_real_offset(self): self.assertIsNotNone(ep._parse_aware("2026-09-21T15:00:00+09:00"))
+ def test_null_offset_datetime_is_not_valid_now(self):
+  bad=datetime(2026,9,21,15,0,tzinfo=NullOffsetTZ())
+  self.assertIsNone(bad.utcoffset())
